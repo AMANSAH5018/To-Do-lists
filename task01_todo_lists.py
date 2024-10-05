c9eddfd57b3d6bfe710df1d2b8eee9def827f4be
AMAN SAH <84163404+AMANSAH5018@users.noreply.github.com>
2024-10-05 12:35:01 +0530

    Add files via upload
    By keeping such a list, you ensure that your tasks are written down all in one place so you don't forget anything important. And by prioritizing tasks, you plan the order in which you'll do them so that you can tell what needs your immediate attention, and what you can leave until later.

---
+++ task01_todo_lists.py
@@ -0,0 +1,46 @@
+def main():
+    tasks = []
+    
+    while True:
+        print("\n ==== To-Do List ====")
+        print("1. Add task")
+        print("2. Show task")
+        print("3. Mark as done")
+        print("4. Exit")
+        
+        choice = input("Enter your choice : ")
+        
+        if choice == "1":
+            print()
+            n_tasks = int(input("Enter the number of tasks to add : "))
+            
+            for i in range(n_tasks):
+                task = input("Enter the task : ")
+                tasks.append({"task": task, "done": False})
+                print("Task added to task list.")
+                
+        elif choice == "2":
+            print("\nTasks :")
+            for index, task in enumerate(tasks):
+                status = "Done" if task["done"] else  "Not Done"
+                print(f"{index+1}. {task['task']} - {status}")
+                
+        elif choice == "3":
+            task_index = int(input("Enter the task number to mark as done : ")) - 1
+            
+            if 0 <= task_index < len(tasks):
+                tasks[task_index-1]["done"] = True
+                print("Task marked as done.")
+            else:
+                print("Invalid task number ...!")
+
+        elif choice == "4":
+            print("Exit from To-Do list")
+            break
+        else:
+            print("Invalid choice number... Please try again.")
+                
+                          
+if __name__ == "__main__":
+    main()
+    
